41924e7979fece69ffb7347a676cfce58246691d
minhdq0709 <doquangminh0709@gmail.com>
2023-03-17 13:18:32 +0700

    a

--- Model/Mysql_Model.js
+++ Model/Mysql_Model.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "192.168.23.22",
-  user: "minhdq",
-  password: "wgy2FdMt0rXfcmCWGSqa",
+  user: "FacebookDb_2_207",
+  password: "h5T8jPiwF72kQfPenka7",
   database: "FacebookDb",
   connectionLimit: 10,
+  port: 3306
 });
 
 module.exports = pool;
